34feba6f62323eee7489751f5175eebd52ae9014
Charles Szilagyi <k@szil.me>
2024-12-07 23:10:09 +0000

    chore: Add Storybook

--- workloads/web/src/routes/+layout.svelte
+++ workloads/web/src/routes/+layout.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
 	/** THIS IS THE TOP LEVEL LAYOUT */
 
-	import './css/reset.css'
-	import './css/open-props.min.css'
-	import './css/theme.css'
-	import './css/global.css'
-	import './css/composition.css'
-	import './css/utility.css'
-	import './css/blocks/block.css'
-	import './css/form.css'
-	import './css/exceptions.css'
+	import '$lib/css/index.css'
 
 	const {children} = $props()
 </script>
---
+++ workloads/web/src/stories/Form.stories.svelte
@@ -0,0 +1,48 @@
+<script module>
+	import {fakerEN_GB as faker} from '@faker-js/faker'
+	import Autocomplete from '$lib/autocomplete/Autocomplete.svelte'
+	import '$lib/css/index.css'
+	import {defineMeta} from '@storybook/addon-svelte-csf'
+
+	const {Story} = defineMeta({
+		title: 'Form',
+	})
+
+	const options = faker.helpers.multiple(() => faker.location.city(), {count: 25})
+</script>
+
+<Story name="Form">
+	<div class="flow">
+		<div>
+			<h1>Input</h1>
+
+			<h2>Text</h2>
+
+			<div class="flow">
+				<input type="text" placeholder="Placeholder..." />
+				<input type="text" value="Input value" />
+			</div>
+		</div>
+
+		<div>
+			<h1>Select</h1>
+
+			<div class="select">
+				<select>
+					<option value="1">Option 1</option>
+					<option value="2">Option 2</option>
+				</select>
+			</div>
+		</div>
+
+		<div>
+			<h1>Autocomplete</h1>
+
+			<h2>Towns</h2>
+			<Autocomplete {options} placeholder="Pick a town from the list" clearable />
+
+			<h2>Empty</h2>
+			<Autocomplete options={[]} />
+		</div>
+	</div>
+</Story>
