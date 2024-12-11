bf4859f9e7b0a9249575977fc97dd83cd7734667
Charles Szilagyi <k@szil.me>
2024-12-11 22:05:04 +0000

    chore: Clean up town data
    - Deleted the towns.sql file from the repo (and history),
      and all references to it
    - Added instructions to the repository README
    - Housekeeping

---
+++ modules/repository/towns/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Verify that the DATABASE_URL is set
+if [ -z "$DATABASE_URL" ]; then
+    echo "DATABASE_URL is not set"
+    exit 1
+fi
+
+# Verify that the towns.sql file exists
+if [ ! -f towns.sql ]; then
+    echo "towns.sql file does not exist - see README.md"
+    exit 1
+fi
+
+docker run \
+    --rm \
+    -v "$(pwd)":/data \
+    -w /data \
+    --network host \
+    -e DATABASE_URL=$DATABASE_URL \
+    postgres bash -c 'psql $DATABASE_URL -f towns.sql'
